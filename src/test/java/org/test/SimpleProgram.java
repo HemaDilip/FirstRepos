e8976a347fb5e5086fd9f7d5a88f4c77da010afa
HemaDilip <hemamaliniaml@gmail.com>
2020-10-24 15:40:12 +0530

    SubMaster Task

--- src/test/java/org/test/SimpleProgram.java
+++ src/test/java/org/test/SimpleProgram.java
@@ -5,6 +5,9 @@ public class SimpleProgram {
 	public static void main(String[] args) {
 
 		System.out.println("To Copy Master");
+		
+		System.out.println("Sub Master");
+		System.out.println("Sub Master created in FirstRepos");
 	}
 
 }
