44329356e1971dd40d196f29ba85d46d3a40aacc
HemaDilip <hemamaliniaml@gmail.com>
2020-10-24 16:01:50 +0530

    CopyMaster Updated

--- src/test/java/org/test/SimpleProgram.java
+++ src/test/java/org/test/SimpleProgram.java
@@ -5,6 +5,10 @@ public class SimpleProgram {
 	public static void main(String[] args) {
 
 		System.out.println("To Copy Master");
+		
+		System.out.println("New Line in Copy Master");
+		System.out.println("Second Line");
+		System.out.println("Third Line");
 	}
 
 }
